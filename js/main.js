8bcfda53e6b8720092be5b04b092a2cf6e2a39c0
GarenLiang <ideabreeze@gmail.com>
2017-04-10 23:46:12 -0700

    Switch Finish

--- js/main.js
+++ js/main.js
@@ -2,28 +2,93 @@
 var GameState = {
   //load the game assets before the game starts
   preload: function() {
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('chicken', 'assets/images/chicken.png');
-    this.load.image('horse', 'assets/images/horse.png');
-    this.load.image('pig', 'assets/images/pig.png');
-    this.load.image('sheep', 'assets/images/sheep3.png');
+    this.game.load.image('background', 'assets/images/background.png');
+    this.game.load.image('arrow', 'assets/images/arrow.png');
+    this.game.load.image('chicken', 'assets/images/chicken.png');
+    this.game.load.image('horse', 'assets/images/horse.png');
+    this.game.load.image('pig', 'assets/images/pig.png');
+    this.game.load.image('sheep', 'assets/images/sheep3.png');
+
   },
   //executed after everything is loaded
   create: function() {
-    this.background = this.game.add.sprite(0, 0, 'background');
-    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-    this.chicken.anchor.setTo(0.5, 0.5);
-    this.chicken.scale.setTo(2,1);
 
-    this.horse = this.game.add.sprite(120, 10, 'horse');
-    this.horse.scale.setTo(0.5);
+    //scaling options
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+    //have the game centered horizontally
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
+    //create a sprite for the background
+    this.background = this.game.add.sprite(0, 0, 'background')
+
+    var animalData = [
+      {key: 'chicken', text: 'CHICKEN'},
+      {key: 'horse', text: 'HORSE'},
+      {key: 'pig', text: 'PIG'},
+      {key: 'sheep', text: 'SHEEP'}
+    ];
+
+    this.animals = this.game.add.group();
+    var self = this;
+    var animal;
+    animalData.forEach(function (element) {
+      animal = self.animals.create(-1000, self.game.world.centerY, element.key);
+      animal.customParams = {text: element.text};
+      animal.anchor.setTo(0.5);
+
+      animal.inputEnabled = true;
+      animal.input.pixelPerfectClick = true;
+      animal.events.onInputDown.add(self.animateAnimal, self);
+
+    });
+    this.currentAnimal = this.animals.next();
+    this.currentAnimal.position.set(this.game.world.centerX, this.game.world.centerY)
+    //left arrow
+    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = {direction: -1};
+
+    //left arrow allow user input
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+    //right arrow
+    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = {direction: 1};
+
+    //right arrow user input
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
+
   },
   //this is executed multiple times per second
   update: function() {
   },
+  animateAnimal: function(sprite, event) {
+      console.log('animate animal');
+  },
+  switchAnimal: function(sprite, event) {
+    var newAnimal, endX;
+    if(sprite.customParams.direction > 0) {
+      newAnimal = this.animals.next();
+      endX = 640 + this.currentAnimal.width/2;
+    }
+    else {
+      newAnimal = this.animals.previous();
+      endX = -this.currentAnimal.width/2;
+    }
+      this.currentAnimal.x = endX;
+      newAnimal.x = this.game.world.centerX;
+      this.currentAnimal = newAnimal;
+    }
+  };
 
-
-};
 
 //initiate the Phaser framework
 var game = new Phaser.Game(640, 360, Phaser.AUTO);
